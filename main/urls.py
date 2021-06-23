284a6411614c5f273e68720cb4c93c1822b8e7f8
Your Name <Ebolotov904@gmail.com>
2021-06-23 15:08:46 +0600

    API

---
+++ main/urls.py
@@ -0,0 +1,16 @@
+# from django.urls import path
+#
+# from main import views
+#
+#
+# urlpatterns = [
+#     path('categories/', views.CategoryListView.as_view(), name='categories-list'),
+#     path('posts/', views.PostView.as_view(), name='posts-list'),
+#     path('posts-detail/<int:pk>', views.PostDetailView.as_view(), name='posts-detail'),
+#     # path('post-create/', views.PostCreateView.as_view(), name='post-create'),
+#     path('posts-update/<int:pk>', views.PostUpdateView.as_view()),
+#     path('posts-delete/<int:pk>', views.PostDeleteView.as_view()),
+#
+# ]
+
+
